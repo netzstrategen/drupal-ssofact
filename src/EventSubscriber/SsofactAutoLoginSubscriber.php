b10999874696ab209245946e4bba02314d069e9d
Daniel Kudwien <daniel@netzstrategen.com>
2018-04-27 10:03:48 +0200

    Added auto-login for unauthenticated users having SSO server cookie.

---
+++ src/EventSubscriber/SsofactAutoLoginSubscriber.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\ssofact\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\openid_connect\Plugin\OpenIDConnectClientManager;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Automatically sends users with SSO cookie to SSO server for authentication.
+ */
+class SsofactAutoLoginSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+  * The OpenID Connect client plugin manager.
+   *
+   * @var \Drupal\openid_connect\Plugin\OpenIDConnectClientManager
+   */
+  protected $pluginManager;
+
+  /**
+   * The curren user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs a new SsofactAutoLoginSubscriber object.
+   */
+  public function __construct(
+    ConfigFactoryInterface $config_factory,
+    OpenIDConnectClientManager $plugin_manager,
+    AccountProxyInterface $current_user
+  ) {
+    $this->configFactory = $config_factory;
+    $this->pluginManager = $plugin_manager;
+    $this->currentUser = $current_user;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // Run after:
+    // - 300: AuthenticationSubscriber (to get current_user)
+    // - 30: MaintenanceModeSubscriber (only local accounts may login during
+    //   maintenance)
+    // Run before:
+    // - 27: DynamicPageCacheSubscriber
+    $events[KernelEvents::REQUEST][] = ['onKernelRequestAuthenticate', 29];
+    return $events;
+  }
+
+  /**
+   * Redirects requests of unauthenticated users having a RF_OAUTH_SERVER cookie.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   */
+  public function onKernelRequestAuthenticate(GetResponseEvent $event) {
+    if ($this->currentUser->isAnonymous() && $event->getRequest()->cookies->has('RF_OAUTH_SERVER')) {
+      $client_config = $this->configFactory->get('openid_connect.settings.ssofact')->get('settings');
+      $client = $this->pluginManager->createInstance('ssofact', $client_config);
+
+      $response = $client->authorize();
+      $target_url = $response->getTargetUrl();
+      $target_url .= '&' . http_build_query([
+        'target' => $event->getRequest()->getRequestUri(),
+      ]);
+
+      $response->setTrustedTargetUrl($target_url);
+      $response->setStatusCode(307);
+      $event->setResponse($response);
+    }
+  }
+
+}
