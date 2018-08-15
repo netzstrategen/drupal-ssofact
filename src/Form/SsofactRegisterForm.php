d5a8d1bd5eb3e2cb67f1fb28c4214db6ea9d1343
Julià Mestieri <juli@netzstrategen.com>
2018-08-15 09:32:11 +0200

    Added 1-article-test.

--- src/Form/SsofactRegisterForm.php
+++ src/Form/SsofactRegisterForm.php
@@ -85,22 +85,22 @@ class SsofactRegisterForm extends FormBase implements ContainerInjectionInterfac
     if (!$client_config->get('enabled')) {
       return $form;
     }
-    // Use custom redirect_uri for client-side login form, which does not check
-    // state token.
+
     $redirect_uri = Url::fromRoute('ssofact.redirect_login', [
       'client_name' => 'ssofact',
     ], [
       'absolute' => TRUE,
       'language' => \Drupal::languageManager()->getLanguage(LanguageInterface::LANGCODE_NOT_APPLICABLE),
       'query' => [
-        'target' => Url::fromUri('internal:/')->toString(),
+        'destination' => Url::fromRoute('<current>')->toString(),
       ],
     ])->toString();
 
+    $server_domain = $client_config->get('settings.server_domain');
     $client_config = $client_config->get('settings');
     $client = $this->pluginManager->createInstance('ssofact', $client_config);
     $endpoints = $client->getEndpoints();
-    $authorize_uri = $endpoints['user_create'] . '?' . http_build_query([
+    $authorize_uri = $endpoints['authorization'] . '?' . http_build_query([
       'client_id' => $client_config['client_id'],
       'response_type' => 'code',
       'scope' => '',
@@ -120,18 +120,31 @@ class SsofactRegisterForm extends FormBase implements ContainerInjectionInterfac
         'autofocus' => 'autofocus',
       ],
     ];
-    $form['redirect_url'] = [
-      '#type' => 'hidden',
-      // @todo Use current path.
-      '#value' => Url::fromUri('internal:/')->toString(),
-    ];
-    $form['node_id'] = [
+
+    $form['article_test'] = [
       '#type' => 'hidden',
       '#value' => $this->routeMatch->getRawParameter('node'),
     ];
 
+    $form['privacy'] = [
+      '#type' => 'checkbox',
+      '#value' => $this->t('I accept terms and conditions'),
+      '#return_value' => '1',
+    ];
+
+    // Hidden field with value "1" to trigger special registration form behavior for 1-article-test.
+    $form['_qf__registerForm'] = [
+      '#type' => '1',
+      '#value' => $this->routeMatch->getRawParameter('node'),
+    ];
+
+    $form['#action'] = 'https://' . $server_domain . '/registrieren.html?' . http_build_query([
+      'next' => $authorize_uri,
+    ]);
+
     $form['actions'] = ['#type' => 'actions'];
     $form['actions']['submit'] = ['#type' => 'submit', '#value' => $this->t('Sign up')];
+
     return $form;
   }
 
@@ -139,36 +152,12 @@ class SsofactRegisterForm extends FormBase implements ContainerInjectionInterfac
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    $client_config = $this->config('openid_connect.settings.ssofact')->get('settings');
-    $client = $this->pluginManager->createInstance('ssofact', $client_config);
-    $response = $client->createUser($form_state->getValue('email'));
-
-    if ($response['statuscode'] !== 200) {
-      foreach ($response['userMessages'] as $error_message) {
-        $form_state->setError($form['email'], $error_message);
-      }
-    }
-    if (empty($response['userId'])) {
-      $form_state->setError($form['email'], $this->t('Unable to register your account. Please contact our support.'));
-    }
-    else {
-      $form_state->setTemporaryValue('userId', $response['userId']);
-    }
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    if ($sub = $form_state->getTemporaryValue('userId')) {
-      // @todo Redirect to SSO login?
-    }
-    // openid_connect_save_destination();
-
-    // $scopes = $this->claims->getScopes();
-    // $_SESSION['openid_connect_op'] = 'login';
-    // $response = $client->authorize($scopes, $form_state);
-    // $form_state->setResponse($response);
   }
 
 }
